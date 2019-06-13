4e68ee662d5a97174348dc722e75bedb73f1efa2
swomics <44398000+swomics@users.noreply.github.com>
2019-06-13 12:00:44 +0100

    Update 00_fastqc_raw_modern.sh

--- 00_fastqc_raw_modern.sh
+++ 00_fastqc_raw_modern.sh
@@ -7,4 +7,4 @@
 
 # Creates submission script to check quality of demultiplexed raw reads using fastqc
 
-/panfs/panasas01/bisc/aj18951/bristol-velocity/AJvR_VelocityPipeline/wrapper/00_parallel_fastqc_bcp3.sh -i 00_raw_reads_modern/ -o /00_raw_reads_modern/ -n 1 -t 8 -m 4;
+00_parallel_fastqc_barkla.sh -i 00_raw_reads_modern/ -o /00_raw_reads_modern/ -n 1 -t 8 -m 4;
